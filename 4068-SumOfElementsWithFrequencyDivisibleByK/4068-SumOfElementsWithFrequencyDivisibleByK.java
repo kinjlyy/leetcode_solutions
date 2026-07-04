5fca610d5a33211c0ab83f73da532e8d14b29a64
Kinjal Gupta <gkinjal81@gmail.com>
2026-07-04 19:40:50 +0530

    Create 4068-SumOfElementsWithFrequencyDivisibleByK

---
+++ 4068-SumOfElementsWithFrequencyDivisibleByK/4068-SumOfElementsWithFrequencyDivisibleByK.java
@@ -0,0 +1,16 @@
+// Last updated: 7/4/2026, 7:40:48 PM
+class Solution {
+    public int sumDivisibleByK(int[] nums, int k) {
+        int freq[]=new int[101];
+        for(int i:nums){
+            freq[i]++;
+        }
+        int sum=0;
+        for(int i=0;i<101;i++){
+            if(freq[i]%k==0&&freq[i]!=0){
+                sum+=(i*freq[i]);
+            }
+        }
+        return sum;
+    }
+}
