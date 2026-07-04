31b903c175822ee3be5ec0a66a5b55b4c8d2e461
Kinjal Gupta <gkinjal81@gmail.com>
2026-07-04 19:42:52 +0530

    Create 3581-TheTwoSneakyNumbersOfDigitville

---
+++ 3581-TheTwoSneakyNumbersOfDigitville/3581-TheTwoSneakyNumbersOfDigitville.java
@@ -0,0 +1,14 @@
+// Last updated: 7/4/2026, 7:42:50 PM
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int n = nums.length;
+        boolean[] a = new boolean[n];
+        int[] ar = new int[2];
+        int k = 0;
+        for(int i = 0;i<n;i++){
+            if(a[nums[i]]==true) ar[k++] = nums[i];
+            else a[nums[i]] = true;
+        }
+        return ar;
+    }
+}
