b58f475313a4dd86140fe22c3b6b6d3c813a21d3
Kinjal Gupta <gkinjal81@gmail.com>
2026-07-24 14:53:31 +0530

    File updated at 7/24/2026, 2:53:31 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,35 +1,30 @@
-// Last updated: 7/24/2026, 2:52:34 PM
+// Last updated: 7/24/2026, 2:53:31 PM
 1class Solution {
 2    public int search(int[] nums, int target) {
-3        if (nums == null || nums.length == 0) return -1;
-4        int n = nums.length;
-5        int pivot = findPivot(nums);
-6        int res = binarySearch(nums, 0, pivot - 1, target);
-7        if (res != -1) return res;
-8        return binarySearch(nums, pivot, n - 1, target);
-9    }
-10
-11    private int findPivot(int[] nums) {
-12        int l = 0, r = nums.length - 1;
-13        while (l < r) {
-14            int mid = l + (r - l) / 2;
-15            if (nums[mid] > nums[r]) {
-16                l = mid + 1;
-17            } else {
-18                r = mid;
-19            }
-20        }
-21        return l;
-22    }
-23
-24    private int binarySearch(int[] nums, int left, int right, int target) {
-25        while (left <= right) {
-26            int mid = left + (right - left) / 2;
-27            if (nums[mid] == target) return mid;
-28            else if (nums[mid] < target) left = mid + 1;
-29            else right = mid - 1;
-30        }
-31        return -1;
-32    }
-33}
-34
+3        int peak=peak(nums);
+4        if(target<=nums[nums.length-1]) return binary(nums,peak, nums.length-1, target);
+5        else return binary(nums,0,peak-1, target);
+6
+7    }
+8    public int peak(int[] nums){
+9        int lo=0;
+10        int hi=nums.length-1;
+11        
+12       while(lo<hi){
+13        int mid=lo+(hi-lo)/2;       
+14        if(nums[mid]>nums[hi]) lo=mid+1;
+15        else hi=mid;
+16       }
+17       return lo;
+18    }
+19    private int binary(int[] nums, int left, int right, int target) {
+20        while (left <= right) {
+21            int mid = left + (right - left) / 2;
+22            if (nums[mid] == target) return mid;
+23            else if (nums[mid] < target) left = mid + 1;
+24            else right = mid - 1;
+25        }
+26        return -1;
+27    }
+28}
+29
